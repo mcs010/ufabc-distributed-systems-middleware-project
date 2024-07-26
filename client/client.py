057f5050000b17fee74793bd3c5a3d4f918bc921
mcs010 <math.cds10@gmail.com>
2024-07-25 21:13:28 -0300

    feat(client): add loop for file sending and minor changes

--- client/client.py
+++ client/client.py
@@ -1,30 +1,42 @@
 from socket import *
+import os
 
 # Address and port
 server = "127.0.0.1"
 port = 43210
+transfer_size = 1024
+size_status = 0
 
-filepath = input("Digite o caminho do arquivo: ")
+filename = input("Digite o nome do arquivo: ")
+file_size = os.path.getsize(filename)
 
-file = open(filepath, "rb")
-
-data = file.read(1024)
+file = open(filename, "rb")
 
 obj_socket = socket(AF_INET, SOCK_STREAM)
 
+# Conecta ao host
 obj_socket.connect((server, port))
 
-while data:
-    print("Enviando arquivo...")
-    obj_socket.sendall(data)
-    data = file.read(1024)
-
-file.close()
+while True:
+    try:
+        data = file.read(transfer_size) 
+        while data:
+            print("Enviando arquivo...")
+            obj_socket.sendall(data)
+            data = file.read(transfer_size)
+            size_status += transfer_size
+            print(f"Enviando {size_status}/{file_size}")
+        file.close()
+        break
+    except:
+        print("Erro ao enviar o arquivo!!!")
 
 obj_socket.send(b"Enviado...")
 
 print("ARQUIVO ENVIADO")
-print(obj_socket.recv(1024))
+print(obj_socket.recv(transfer_size))
 
 obj_socket.shutdown(2)
+
+# Fechar conexão
 obj_socket.close()
