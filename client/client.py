1aabea5fd0219d3de307905b0bce32b4f001ae63
mcs010 <math.cds10@gmail.com>
2024-07-30 19:33:57 -0300

    fix(): fix bugs on file upload and download for client and server sides

--- client/client.py
+++ client/client.py
@@ -1,11 +1,10 @@
 from socket import *
 import os
-import time
 
 # Address and port
 IP_ADDRESS = "127.0.0.1"
 PORT = 43210
-TRANSFER_SIZE = 1024
+BUFFER_SIZE = 1024
 FILENAME = input("Digite o nome do arquivo: ")
 print("Nome arquivo: ", FILENAME)
 
@@ -16,19 +15,36 @@ client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((IP_ADDRESS, PORT))
 
 # Acessando arquivo
-file = open(FILENAME, "r")
+if ".txt" in FILENAME:
+    file = open(FILENAME, "r")
 
-# Armazenando dados do arquivo
-data = file.read()
+    # Armazenando dados do arquivo
+    data = file.read()
 
-# Enviando nome do arquivo ao servidor
-print("Enviando nome do arquivo....")
-client_socket.send(FILENAME.encode('utf-8'))
+    # Enviando nome do arquivo ao servidor
+    print("Enviando nome do arquivo....")
+    client_socket.send(FILENAME.encode('utf-8'))
 
-# Enviando arquivo ao servidor
-print("Enviando arquivo....")
-client_socket.sendall(data.encode())
+    # Enviando arquivo ao servidor
+    print("Enviando arquivo....")
+    client_socket.sendall(data.encode())
 
-file.close()
+    file.close()
+
+elif ".png" in FILENAME:
+    with open(FILENAME, "rb") as file:
+
+        # Armazenando dados do arquivo
+        data = file.read(BUFFER_SIZE)
+
+        # Enviando nome do arquivo ao servidor
+        print("Enviando nome do arquivo....")
+        client_socket.send(FILENAME.encode('utf-8'))
+
+        while data:
+            # Enviando arquivo ao servidor
+            print("Enviando arquivo....")
+            client_socket.send(data)
+            data = file.read(BUFFER_SIZE)
 
 client_socket.close()
--- server/server1.py
+++ server/server1.py
@@ -1,10 +1,52 @@
 from socket import *
 import os
 
-client_address = "127.0.0.1" #localhost
-port = 43210
+# Address and port
+IP_ADDRESS = "127.0.0.1"
+PORT = 43210
+BUFFER_SIZE = 1024
 
-TRANSFER_SIZE = 1024
+# Iniciando socket TCP
+server_socket = socket(AF_INET, SOCK_STREAM)
+
+server_socket.bind((IP_ADDRESS, PORT))
+
+# Servidor esperando conexão do client
+server_socket.listen()
+print("Servidor aguardando conexao....")
+
+while True:
+    # Servidor aceita conexao do client
+    conn, addr = server_socket.accept()
+    print(f"Conectado ao client {conn}/{addr}")
+
+    # Recebendo nome do arquivo
+    filename = conn.recv(BUFFER_SIZE).decode('utf-8')
+    print(f"Recebendo nome do arquivo: {filename}")
+    
+    if ".txt" in filename:
+        file = open(filename, "w")
+        print(f"Nome do arquivo recebido")
+
+        # Recebendo dados do arquivo do client
+        print("Recebendo dados do arquivo....")
+        data = conn.recv(BUFFER_SIZE).decode()
+        file.write(data)
+        print("Arquivo recebido")
+
+        file.close()
+
+        conn.close()
+
+    elif ".png" in filename:
+        with open(filename, "wb") as file:
+            received_data = conn.recv(BUFFER_SIZE)
+
+            while data:
+                file.write(received_data)
+                received_data = conn.recv(BUFFER_SIZE)
+
+            print("Arquivo recebido......")
 
 obj_socket = socket(AF_INET, SOCK_STREAM)
 
