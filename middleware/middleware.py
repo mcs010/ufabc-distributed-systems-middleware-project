ac171e3c3827cd0c147b85697b66038e77daebda
mcs010 <math.cds10@gmail.com>
2024-08-01 17:48:39 -0300

    feat(middleware): add middleware file

---
+++ middleware/middleware.py
@@ -0,0 +1,16 @@
+from socket import *
+
+# Address and port
+IP_ADDRESS = "127.0.0.1"
+PORT = 43210
+BUFFER_SIZE = 1024
+
+def receive_from_client():
+    """
+    Receives file from client socket
+    """
+
+def send_to_server():
+    """
+    Sends file to server socket
+    """
