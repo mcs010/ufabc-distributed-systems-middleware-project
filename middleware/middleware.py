4ff53dab418e1e1a238664390ecafb1dbd846c4d
mcs010 <math.cds10@gmail.com>
2024-08-01 18:13:48 -0300

    feat(middleware): add functions for client and server connections

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -5,10 +5,34 @@ IP_ADDRESS = "127.0.0.1"
 PORT = 43210
 BUFFER_SIZE = 1024
 
-def receive_from_client():
+def connect_to_client():
+    """
+    Funçao para conectar cliente ao middleware
+    Retorna o socket object
+    """
+
+def receive_from_client(IP_ADDRESS, PORT):
     """
     Receives file from client socket
     """
+    # Criando socket TCP
+    middleware_socket = socket(AF_INET, SOCK_STREAM)
+
+    # Conectando ao servidor
+    middleware_socket.connect((IP_ADDRESS, PORT))
+
+def connect_to_server(IP_ADDRESS, PORT):
+    """
+    Funçao para conectar middleware ao servidor
+    Retorna o socket object
+    """
+    # Criando socket TCP
+    middleware_socket = socket(AF_INET, SOCK_STREAM)
+
+    # Conectando ao servidor
+    middleware_socket.connect((IP_ADDRESS, PORT))
+
+    return middleware_socket
 
 def send_to_server():
     """
